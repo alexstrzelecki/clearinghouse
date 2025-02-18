699aedfdfef2b8cfbc016178d83763a1ae66fe37
Strzelecki Alexander <alexstrzelecki@squareup.com>
2025-02-18 17:53:55 -0500

    Skeleton of orders endpoint

--- clearinghouse/routers/orders.py
+++ clearinghouse/routers/orders.py
@@ -1,9 +1,52 @@
+from typing import List
 
 import schwabdev
 from fastapi import APIRouter, Depends, HTTPException
+import msgspec
+from starlette import status
 
 from clearinghouse.dependencies import SchwabService
+from clearinghouse.models.request import Order
+from clearinghouse.models.schwab import (
+    EquityPosition,
+    Transaction,
+)
 
 
 def create_order_endpoints(schwab_service: SchwabService):
     order_router = APIRouter()
+
+    @order_router.get(
+        "/orders",
+        status_code=status.HTTP_200_OK,
+        response_model=List[Transaction],
+    )
+    def get_orders(client: schwab_service):
+        pass
+
+    # place order
+
+
+    # order details
+
+
+
+    # cancel order
+
+
+
+    # get all transactions
+
+
+
+    # get single quote
+
+
+    # bulk orders
+
+
+    # percentage change
+
+
+    # bulk percentage change
+
---
+++ clearinghouse/services/orders_service.py
@@ -0,0 +1,12 @@
+
+
+def get_orders():
+    pass
+
+
+def place_order():
+    pass
+
+
+def place_bulk_orders():
+    pass
