979e8cd4ac5f81df127ee4e2152597009b1dc2c4
Strzelecki Alexander <alexstrzelecki@squareup.com>
2025-02-18 17:45:57 -0500

    Add pydantic model for order

---
+++ clearinghouse/models/request.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from enum import Enum
+
+
+class OrderOperation(str, Enum):
+    buy = "buy"
+    sell = "sell"
+    short = "short"
+    close = "close"
+
+
+class OrderType(str, Enum):
+    market = "market"
+    limit = "limit"
+    stop = "stop"
+
+
+class OrderAmountType(str, Enum):
+    percentage = "percentage"
+    shares = "shares"
+
+
+class OrderDuration(str, Enum):
+    """
+    For how long to keep the order open
+    TODO: add the remaining options
+    """
+    day = "day"
+    gtc = "gtc"
+
+
+class Order(BaseModel):
+    ticker: str
+    price: float
+    operation: OrderOperation
+    order_type: OrderType
+    amount: int | float
+    amount_type: OrderAmountType
+    duration: OrderDuration
